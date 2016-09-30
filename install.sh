ca2489c88e393c9d7eb2d967a7553f784165f43a
cost <cstern19@gmail.com>
2016-09-30 00:58:07 -0400

    Initial commit

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+sudo apt-get update  # To update package lists
+sudo apt-get install numix-gtk-theme -y
+sudo apt-get install sublime-text -y
+sudo apt-get install google-chrome-stable -y
+sudo apt-get install spotify-client -y
+sudo apt-get install htop -y
+sudo apt-get install caffeine -y
+sudo apt-get install okular -y
+sudo apt-get install weechat -y
+sudo apt-get install sbt -y
+sudo apt-get install scala -y
+sudo apt-get install firefox -y
+sudo apt-get install p7zip -y
+sudo apt-get install kodi -y
+sudo apt-get install virtualbox -y
+cd ~ && wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf
+~/.dropbox-dist/dropboxd
+
