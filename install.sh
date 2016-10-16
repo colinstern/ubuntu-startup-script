d0ca3274880d1e7df91ad87931c2315fa41657f1
cost <cstern19@gmail.com>
2016-10-16 18:42:00 -0400

    Added Ubuntu Tweak.

--- install.sh
+++ install.sh
@@ -16,4 +16,5 @@ sudo apt-get install kodi -y
 sudo apt-get install virtualbox -y
 cd ~ && wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf
 ~/.dropbox-dist/dropboxd
-
+cd ~/Downloads/ && wget https://launchpad.net/ubuntu-tweak/0.8.x/0.8.5/+download/ubuntu-tweak_0.8.5-1_all.deb
+sudo dpkg -i ~/Downloads/ubuntu-tweak_*.deb; sudo apt-get -f install
