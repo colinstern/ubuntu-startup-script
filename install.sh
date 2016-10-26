6b7cf0274ea8e9098c1a9a83d2b7ae13f42992f0
cost <cstern19@gmail.com>
2016-10-26 02:02:21 -0400

    Added f.lux

--- install.sh
+++ install.sh
@@ -18,3 +18,6 @@ cd ~ && wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf
 ~/.dropbox-dist/dropboxd
 cd ~/Downloads/ && wget https://launchpad.net/ubuntu-tweak/0.8.x/0.8.5/+download/ubuntu-tweak_0.8.5-1_all.deb
 sudo dpkg -i ~/Downloads/ubuntu-tweak_*.deb; sudo apt-get -f install
+sudo add-apt-repository ppa:nathan-renniewaldock/flux
+sudo apt-get update
+sudo apt-get install fluxgui
